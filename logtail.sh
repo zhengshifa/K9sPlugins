1029042cded9de1f05db2a9649b2848748f69077
zhengshifa <804410011@qq.com>
2024-10-13 22:01:31 +0800

    批量提交代码更新 - 2024-10-13 22:00:51

--- logtail.sh
+++ logtail.sh
@@ -11,22 +11,25 @@ POD_NAME="$2"
 CONTAINER_NAME="$3"
 CONTEXT="$4"
 
+clear   
 # 计算给定分钟前的时间点
 read -p "请输入要查询多少分钟的日志: " MINUTES
 LOG_TIME=$(date -d "-$MINUTES minutes" --utc +"%Y-%m-%dT%H:%M:%SZ")
+log_file="/tmp/$1_$2_$3_$(date +%Y-%m-%d_%H_%M_%S).log"
 
 # 获取指定pod指定container中指定时间点之后的日志
-kubectl logs $KUBECTL_ARGS $POD_NAME -c $CONTAINER_NAME --context=$CONTEXT --since-time=$LOG_TIME
+kubectl logs $KUBECTL_ARGS $POD_NAME -c $CONTAINER_NAME --context=$CONTEXT --since-time=$LOG_TIME |tee "$log_file"
 echo -e "\n\n\n\n\n\n\n"
 read -p "是否保存日志[y/n]: " SIN
+
 if [ $SIN == 'y' ]; then
-   kubectl logs $KUBECTL_ARGS $POD_NAME -c $CONTAINER_NAME --context=$CONTEXT --since-time=$LOG_TIME > /tmp/$1_$2_$3_$(date +%Y-%m-%d_%H_%M_%S).log
 clear   
-echo "sz /tmp/$1_$2_$3_$(date +%Y-%m-%d_%H_%M_%S).log"
+echo "sz $log_file"
 sleep 4
 else 
    clear
    echo "不保存"
+   rm -f "$log_file"
    sleep 1
    exit
 fi
